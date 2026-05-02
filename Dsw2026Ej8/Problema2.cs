743b5e760b333112d9180843833a76a169b24df1
SantiagoCheble1 <jerecheble@gmail.com>
2026-05-02 11:32:45 -0300

    problema 2 resuelto

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription,int quantity ,double unitPrice)
+        {
+            double total = quantity > 0 ? quantity * unitPrice : 0;
+
+
+            object venta = new
+            {
+                Code = productCode,
+                Product = productDescription,
+                Quantity = quantity,
+                Total = quantity * unitPrice
+            };
+
+            return $"{venta.Code} - {venta.Product} - {venta.Total}";
+        }
+    }
+}
