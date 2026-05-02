d6d48b390ce0b0c68176693c076e4791bded1364
SantiagoCheble1 <jerecheble@gmail.com>
2026-05-02 13:10:29 -0300

    solucion problema 5

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var problema = new Problema5();
+            Sale venta1 = new RetailSale();
+            venta1._monto = 1000;
+            Sale venta2 = new WholesaleSale();
+            venta2._monto = 1000;
+
+            Console.WriteLine(problema.ObtenerImporteFinal(venta1)); 
+            Console.WriteLine(problema.ObtenerImporteFinal(venta2)); 
         }
     }
 }
---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Sale
+    {
+        public decimal _monto;
+        public virtual decimal CalculateTotal()
+        {
+            return _monto;
+        }
+    }
+}
