c68a0d1ef1c125348d3472a4ebc0cd2c4c7cd7a6
SantiagoCheble1 <jerecheble@gmail.com>
2026-05-02 11:32:20 -0300

    problema 1 resuelto

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class  ProductHelper
+    {
+        public String ObtenerEtiquetaProducto(long code,string descripcion, double precio)
+        {
+            return $"[{code}] {descripcion} - {obtenerPrecio(precio)}";
+        }
+
+    }
+}
