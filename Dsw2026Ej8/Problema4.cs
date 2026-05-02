402da832d27ce85913ecc5d226e7f478319e303c
SantiagoCheble1 <jerecheble@gmail.com>
2026-05-02 12:48:45 -0300

    problema 4 resuelto

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidad = 0;
+
+            void ValidarNota(int? nota)
+            {
+                if (nota.HasValue && nota >= 0 && nota <= 10)
+                {
+                    suma += nota.Value;
+                    cantidad++;
+                }
+            }
+
+            ValidarNota(nota1);
+            ValidarNota(nota2);
+            ValidarNota(nota3);
+
+            if (cantidad == 0)
+                return 0;
+
+            double total = (double)suma/ cantidad;
+
+            return total;
+        }
+    }
+}
