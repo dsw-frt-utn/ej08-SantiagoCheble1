a129c9e515f173a51bd1f24693e8ff97552ee4b6
SantiagoCheble1 <jerecheble@gmail.com>
2026-05-02 11:33:15 -0300

    problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public void CompararCopias(int originalValue, ProductHelper Product)
+        {
+            int value = originalValue;
+            value++;
+
+            var producto = Product;
+        }
+    }
+}
